4eb086fa363cc47edbef431914339ed690c0152d
Alexandre Gros <xelaflash@gmail.com>
2020-01-20 13:48:50 -0400

    sliding effect working clean up to do

--- source/assets/javascripts/components/mobile_nav.js
+++ source/assets/javascripts/components/mobile_nav.js
@@ -3,7 +3,7 @@ const sidenavInner = document.querySelector('.sidenav');
 const { body } = document;
 
 function toggleNav() {
-  burger.classList.toggle('is-active');
+  // burger.classList.toggle('is-active');
   sidenavInner.classList.toggle('show-sidenav');
   if (sidenavInner.classList.contains('show-sidenav')) {
     body.classList.add('freezePage');
--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -11,3 +11,13 @@ sidenavTrigger.addEventListener('click', toggleNav);
 sideNavLinks.forEach(function(link) {
   link.addEventListener('click', toggleNav);
 });
+
+const nImg = document.getElementById('n-img');
+const twentyOneImg = document.getElementById('21-img');
+const burger2 = document.querySelector('.hamburger');
+
+function toggleImg() {
+  nImg.classList.toggle('show-letter-n');
+  twentyOneImg.classList.toggle('show-letter-21');
+}
+burger2.addEventListener('click', toggleImg);
