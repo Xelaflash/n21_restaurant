0f9542af5085ad57a986eac630204555298770ba
Alexandre Gros <xelaflash@gmail.com>
2020-01-20 11:22:13 -0400

    sidenav js + styling

---
+++ source/assets/javascripts/components/mobile_nav.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.hamburger');
+const sidenavInner = document.querySelector('.sidenav');
+const { body } = document;
+
+function toggleNav() {
+  burger.classList.toggle('is-active');
+  sidenavInner.classList.toggle('show-sidenav');
+  if (sidenavInner.classList.contains('show-sidenav')) {
+    body.classList.add('freezePage');
+  } else {
+    body.classList.remove('freezePage');
+  }
+}
+
+export { toggleNav };
--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,13 @@
 import '@babel/polyfill';
+
+// components JS files
+import { toggleNav } from './components/mobile_nav';
+
+// Sidenav
+const sidenavTrigger = document.getElementById('sidenav-trigger');
+const sideNavLinks = document.querySelectorAll('.sidenav-link');
+
+sidenavTrigger.addEventListener('click', toggleNav);
+sideNavLinks.forEach(function(link) {
+  link.addEventListener('click', toggleNav);
+});
