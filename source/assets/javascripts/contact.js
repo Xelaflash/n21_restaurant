f7251efd0345850a8e1d81aff7e14f7088b86d1e
florian Dublanc <floriandbc@gmail.com>
2020-01-22 17:15:41 +0100

    contact form

--- source/assets/javascripts/contact.js
+++ source/assets/javascripts/contact.js
@@ -1,71 +1,71 @@
-(function($) {
-  /*= =================================================================
-    [ Validate ] */
-  const name = $('.validate-input input[name="name"]');
-  const email = $('.validate-input input[name="email"]');
-  const subject = $('.validate-input input[name="subject"]');
-  const message = $('.validate-input textarea[name="message"]');
+// (function($) {
+//   /*= =================================================================
+//     [ Validate ] */
+//   const name = $('.validate-input input[name="name"]');
+//   const email = $('.validate-input input[name="email"]');
+//   const subject = $('.validate-input input[name="subject"]');
+//   const message = $('.validate-input textarea[name="message"]');
 
-  $('.validate-form').on('submit', function() {
-    let check = true;
+//   $('.validate-form').on('submit', function() {
+//     let check = true;
 
-    if (
-      $(name)
-        .val()
-        .trim() == ''
-    ) {
-      showValidate(name);
-      check = false;
-    }
+//     if (
+//       $(name)
+//         .val()
+//         .trim() == ''
+//     ) {
+//       showValidate(name);
+//       check = false;
+//     }
 
-    if (
-      $(subject)
-        .val()
-        .trim() == ''
-    ) {
-      showValidate(subject);
-      check = false;
-    }
+//     if (
+//       $(subject)
+//         .val()
+//         .trim() == ''
+//     ) {
+//       showValidate(subject);
+//       check = false;
+//     }
 
-    if (
-      $(email)
-        .val()
-        .trim()
-        .match(
-          /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
-        ) == null
-    ) {
-      showValidate(email);
-      check = false;
-    }
+//     if (
+//       $(email)
+//         .val()
+//         .trim()
+//         .match(
+//           /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+//         ) == null
+//     ) {
+//       showValidate(email);
+//       check = false;
+//     }
 
-    if (
-      $(message)
-        .val()
-        .trim() == ''
-    ) {
-      showValidate(message);
-      check = false;
-    }
+//     if (
+//       $(message)
+//         .val()
+//         .trim() == ''
+//     ) {
+//       showValidate(message);
+//       check = false;
+//     }
 
-    return check;
-  });
+//     return check;
+//   });
 
-  $('.validate-form .input1').each(function() {
-    $(this).focus(function() {
-      hideValidate(this);
-    });
-  });
+//   $('.validate-form .input1').each(function() {
+//     $(this).focus(function() {
+//       hideValidate(this);
+//     });
+//   });
 
-  function showValidate(input) {
-    const thisAlert = $(input).parent();
+//   function showValidate(input) {
+//     const thisAlert = $(input).parent();
 
-    $(thisAlert).addClass('alert-validate');
-  }
+//     $(thisAlert).addClass('alert-validate');
+//   }
 
-  function hideValidate(input) {
-    const thisAlert = $(input).parent();
+//   function hideValidate(input) {
+//     const thisAlert = $(input).parent();
 
-    $(thisAlert).removeClass('alert-validate');
-  }
-})(jQuery);
+//     $(thisAlert).removeClass('alert-validate');
+//   }
+// })(jQuery);
