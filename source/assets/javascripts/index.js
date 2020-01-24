1e5e37b1770c0a6b711c97d549aac3c950e64db8
Alexandre Gros <xelaflash@gmail.com>
2020-01-24 09:39:31 -0400

    navbar on scroll

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -4,9 +4,13 @@ import './components/contact';
 // components JS files
 import { toggleNav, toggleImg } from './components/mobile_nav';
 import { handleFirstTab } from './components/keyboard_focus';
+import { checkScroll } from './components/nav_scroll';
 
 // outline for keyboard user on focus
 window.addEventListener('keydown', handleFirstTab);
+
+// Navbar visible on scroll
+window.addEventListener('scroll', checkScroll);
 
 // Sidenav
 const sidenavTriggers = document.querySelectorAll('.sidenav-trigger');
