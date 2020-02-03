dc62fa35b1ad126e09d13f6dd08c871e4df9d2f5
Alexandre Gros <xelaflash@gmail.com>
2020-02-03 12:54:42 -0400

    map styling and marker

--- source/assets/javascripts/plugins/mapbox.js
+++ source/assets/javascripts/plugins/mapbox.js
@@ -6,19 +6,21 @@ const initMapbox = () => {
   if (mapElement) {
     // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const shop = [5.033759, 43.539678];
+    const shop = [106.690461, 10.780869];
 
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10',
-      center: [5.033759, 43.54], // starting position
-      zoom: 14, // starting zoom
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [106.690461, 10.780869], // starting position
+      zoom: 16, // starting zoom
+      pitch: 60,
+      bearing: 0,
     });
 
     // create the popup
-    const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h5>Emilie Ogier <br> Sophrologie</h5><br><p>2 impasse de l'étang - 13250 Saint Chamas</p>`
-    );
+    const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+    <img class="popup-logo" src="https://res.cloudinary.com/dpra9vwkq/image/upload/v1579271875/Logo.png" alt="N21 Restaurant - Logo"> <br>
+    <h5>N21 Restaurant</h5>`);
 
     // create DOM element for the marker
     const el = document.createElement('div');
@@ -32,6 +34,39 @@ const initMapbox = () => {
 
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
+
+    // The 'building' layer in the mapbox-streets vector source contains building-height
+    // data from OpenStreetMap.
+    map.on('load', function() {
+      // Insert the layer beneath any symbol layer.
+      const { layers } = map.getStyle();
+      let labelLayerId;
+      for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      }
+      map.addLayer(
+        {
+          id: '3d-buildings',
+          source: 'composite',
+          'source-layer': 'building',
+          filter: ['==', 'extrude', 'true'],
+          type: 'fill-extrusion',
+          minzoom: 15,
+          paint: {
+            'fill-extrusion-color': '#aaa',
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': ['interpolate', ['linear'], ['zoom'], 15, 0, 15.05, ['get', 'height']],
+            'fill-extrusion-base': ['interpolate', ['linear'], ['zoom'], 15, 0, 15.05, ['get', 'min_height']],
+            'fill-extrusion-opacity': 0.6,
+          },
+        },
+        labelLayerId
+      );
+    });
   }
 };
 
