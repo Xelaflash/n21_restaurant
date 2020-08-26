f63eb4c4e44a0b687e665e4a59c9cc0831b58d8d
Alexandre Gros <xelaflash@gmail.com>
2020-08-26 12:23:38 -0400

    📝 change sidenav bg and logo color

--- source/assets/javascripts/components/mobile_nav.js
+++ source/assets/javascripts/components/mobile_nav.js
@@ -2,11 +2,16 @@ const sidenavInner = document.querySelector('.sidenav');
 const { body } = document;
 
 function toggleNav() {
+  const burger = document.querySelector('.hamburger');
   sidenavInner.classList.toggle('show-sidenav');
   if (sidenavInner.classList.contains('show-sidenav')) {
     body.classList.add('freezePage');
+    burger.style.backgroundImage =
+      "url('https://res.cloudinary.com/dpra9vwkq/image/upload/v1598455792/burger-black.png')";
   } else {
     body.classList.remove('freezePage');
+    burger.style.backgroundImage =
+      "url('https://res.cloudinary.com/dpra9vwkq/image/upload/v1583333596/burger_white.png')";
   }
 }
 
